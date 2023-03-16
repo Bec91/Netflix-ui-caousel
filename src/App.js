7bd75b3a72bffc2ce6baf006ebe78b645f8c8158
Becca <rkinnee91@hotmail.com>
2023-03-16 16:09:59 -0700

    add slider component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.scss';
 
+// Components
+import Slider from './Components/SliderContainer/SliderContainer';
+
 function App() {
   return (
     <div className="App">
+      <Slider/>
     </div>
   );
 }
---
+++ src/Components/SliderContainer/SliderContainer.js
@@ -0,0 +1,21 @@
+// Styles
+import './SliderContainer.scss'
+
+function Slider() {
+    return (
+        <section className='container'>
+            <div className='container__slider'>
+                <img src="" alt="" className='container__img'/>
+                <img src="" alt="" className='container__img'/>
+                <img src="" alt="" className='container__img'/>
+                <img src="" alt="" className='container__img'/>
+                <img src="" alt="" className='container__img'/>
+                <img src="" alt="" className='container__img'/>
+                <img src="" alt="" className='container__img'/>
+            </div>
+
+        </section>
+    )
+}
+
+export default Slider;
